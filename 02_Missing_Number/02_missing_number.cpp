ce30855f8024762d3136d7f7c0c3d78e98b3661d
theoneandonlygiraffe <theoneandonlygiraffe@web.de>
2024-12-16 21:13:52 +0100

    cleanup

--- 02_Missing_Number/02_missing_number.cpp
+++ 02_Missing_Number/02_missing_number.cpp
@@ -6,39 +6,31 @@ int main(int argc , char* argv[]){
     std::string input;
     std::stringstream stream;
     
-    
     std::getline(std::cin , input);
     const unsigned long n = std::stoi(input);
-    unsigned long* array = new unsigned long[n];
+    unsigned int* array = new unsigned int[n];
     std::getline(std::cin , input);
     stream << input;
     
     
-    for(unsigned int i = 0; i < n - 1; i++) {
+    for(unsigned long i = 0; i < n - 1; i++) {
         std::string temp;
         std::getline(stream, temp, ' ');
         array[i] = std::stoi(temp);
-        //std::cout << array[i] << '\n';
-    }
-    
-    //std::cout << "--" << '\n';
+    }   
     
     unsigned long sum = 0;
-    for(unsigned int i = 0; i < n - 1; i++) {
+    for(unsigned long i = 0; i < n - 1; i++) {
         sum = sum + array[i];
-        //std::cout << sum << '\n';
     }
-    //std::cout << "--" << '\n';
     
     unsigned long shouldbe;
-    if( n % 2 == 0){
-        shouldbe = (n / 2) * (n + 1); //gaus?
-    } else {
-        shouldbe = ((n-1) / 2) * ((n-1) + 1);
-        shouldbe = shouldbe + n ;
+    unsigned long even = n & ~(1);
+    shouldbe = (even >> 1) * (even + 1);
+    if (n & 1) {
+        shouldbe = shouldbe + n;
     }
-    //std::cout << shouldbe << '\n';
-    //std::cout << sum << '\n';
+    
     std::cout << shouldbe - sum << '\n';
         
 }
